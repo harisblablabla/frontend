f3e9b5813c20b2cb942ce9778f0ac10ecd193988
haris <haris.fadhilah21@gmail.com>
2025-04-20 16:10:53 +0700

    refactor(component): change button property (end, start icon) so it could handle onClick independently

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,16 @@
 import React, { ReactNode } from "react";
 
+type iconType = | ReactNode | {
+  icon: ReactNode,
+  onClick: (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => void
+};
+
 interface ButtonProps {
   children: ReactNode; // Button text or content
   size?: "sm" | "md"; // Button size
   variant?: "primary" | "outline"; // Button variant
-  startIcon?: ReactNode; // Icon before the text
-  endIcon?: ReactNode; // Icon after the text
+  startIcon?: iconType; // Icon before the text
+  endIcon?: iconType; // Icon after the text
   onClick?: () => void; // Click handler
   disabled?: boolean; // Disabled state
   className?: string; // Disabled state
@@ -37,17 +42,36 @@ const Button: React.FC<ButtonProps> = ({
 
   return (
     <button
-      className={`inline-flex items-center justify-center font-medium gap-2 rounded-lg transition ${className} ${
-        sizeClasses[size]
-      } ${variantClasses[variant]} ${
-        disabled ? "cursor-not-allowed opacity-50" : ""
-      }`}
+      className={`inline-flex items-center justify-center font-medium gap-2 rounded-lg transition ${className} ${sizeClasses[size]
+        } ${variantClasses[variant]} ${disabled ? "cursor-not-allowed opacity-50" : ""
+        }`}
       onClick={onClick}
       disabled={disabled}
     >
-      {startIcon && <span className="flex items-center">{startIcon}</span>}
+      {startIcon && <span className="flex items-center"
+        onClick={(e) => {
+          if (typeof startIcon === "object" && "onClick" in startIcon) {
+            e.stopPropagation() // prevent button click
+            startIcon.onClick(e)
+          }
+        }}
+      >{typeof startIcon === "object" && "icon" in startIcon ? startIcon.icon : startIcon}</span>}
+
       {children}
-      {endIcon && <span className="flex items-center">{endIcon}</span>}
+
+      {endIcon && (
+        <span
+          className="flex items-center"
+          onClick={(e) => {
+            if (typeof endIcon === "object" && "onClick" in endIcon) {
+              e.stopPropagation(); // prevent button click
+              endIcon.onClick(e);
+            }
+          }}
+        >
+          {typeof endIcon === "object" && "icon" in endIcon ? endIcon.icon : endIcon}
+        </span>
+      )}
     </button>
   );
 };
